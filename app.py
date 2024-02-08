fa5aff4c1d7c2c4cdef5939bd1401f2946f282aa
BakrAsskali <bakrasskali@gmail.com>
2024-02-08 21:04:46 +0100

    Added basic validation

--- app.py
+++ app.py
@@ -1,11 +1,17 @@
-from flask import Flask
+from flask import Flask, request
+import sqlvalidator
 
 app = Flask(__name__)
 
 
-@app.route('/')
-def hello_world():  # put application's code here
-    return 'Hello World!'
+@app.route('/ValidateSQL', methods=['GET'])
+def validate_query():
+    get_body = request.get_json()
+    query = get_body['query']
+    sql_query = sqlvalidator.parse(query)
+    if not sql_query.is_valid():
+        return "Invalid SQL Query"
+    return "Valid SQL Query"
 
 
 if __name__ == '__main__':
