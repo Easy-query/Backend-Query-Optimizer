08f5932962ee8d8ec78057c016d3396702049b19
BakrAsskali <bakrasskali@gmail.com>
2024-02-09 17:15:25 +0100

    switched over to sqlglot

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask, request
-import sqlvalidator
+import sqlglot
+from sqlglot.optimizer import optimize
 
 app = Flask(__name__)
 
@@ -8,10 +9,12 @@ app = Flask(__name__)
 def validate_query():
     get_body = request.get_json()
     query = get_body['query']
-    sql_query = sqlvalidator.parse(query)
-    if not sql_query.is_valid():
-        return "Invalid SQL Query"
-    return "Valid SQL Query"
+    try:
+        sqlglot.transpile(query, read='oracle', pretty='true')
+        print(optimize(query))
+        return {'status': 'success', 'message': 'Query is valid'}
+    except Exception as e:
+        return {'status': 'error', 'message': str(e)}
 
 
 if __name__ == '__main__':
