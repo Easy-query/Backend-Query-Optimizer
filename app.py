136a0294a468a2c46dfdff3096f5274549683ba8
BakrAsskali <bakrasskali@gmail.com>
2024-02-10 18:00:37 +0100

    Updated the code and added the execution plan which is printed

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ from flask import Flask, request
 import sqlglot
 from sqlglot.optimizer import optimize
 import db_util
+from sqlalchemy import text
 
 app = Flask(__name__)
 
@@ -11,15 +12,10 @@ def validate_query():
     get_body = request.get_json()
     query = get_body['query']
     try:
-        sqlglot.transpile(query, read='oracle', pretty='true')
-        print(optimize(query))
         conn = db_util.get_connection()
-        cursor = conn.cursor()
-        cursor.execute('EXPLAIN PLAN FOR ' + query)
-        cursor.execute('SELECT * FROM TABLE(DBMS_XPLAN.DISPLAY)')
-        for row in cursor:
+        explanation = conn.execute(text('EXPLAIN FORMAT=TREE ' + query))
+        for row in explanation:
             print(row)
-        cursor.close()
         conn.close()
         return {'status': 'success', 'message': 'Query is valid'}
     except Exception as e:
--- db_util.py
+++ db_util.py
@@ -1,10 +1,12 @@
-import cx_Oracle
-
+import sqlalchemy
+from sqlalchemy.orm import sessionmaker
 
 def get_connection():
-    username = 'oracle'
-    password = 'oracle'
-    dsn = 'localhost:1521/orcl'
-
-    conn = cx_Oracle.connect(user=username, password=password, dsn=dsn)
-    return conn
+    username = 'bakr'
+    password = 'bakr'
+    hostname = '34.31.91.76'
+    port = '3306'
+    database = 'optimisation_db'
+    engine = sqlalchemy.create_engine('mysql+pymysql://' + username + ':' + password + '@' + hostname + ':' + port + '/' + database)
+    session = sessionmaker(bind=engine)
+    return session()
