247be83dd7cf3913e28dc8d43f1a6f5c36276655
BakrAsskali <bakrasskali@gmail.com>
2024-02-12 14:17:51 +0100

    no need forwebsockets

--- app.py
+++ app.py
@@ -1,6 +1,5 @@
 from flask import Flask, request
 from flask_cors import CORS
-from flask_socketio import SocketIO
 from sqlalchemy import text
 
 import db_util
@@ -9,24 +8,6 @@ from model import search
 app = Flask(__name__)
 
 CORS(app)
-socketio = SocketIO(app, cors_allowed_origins="*")
-
-
-@SocketIO.on(message="query", self=socketio)
-def get_optimized_query_socket(query):
-    try:
-        execution_plan = None
-        conn = db_util.get_connection()
-        explanation = conn.execute(text('EXPLAIN FORMAT=TREE ' + query))
-        for row in explanation:
-            execution_plan = row[0]
-        conn.close()
-        result_df = search(query)
-        optimized_queries = result_df["optimized_query"].tolist()
-        return {'status': 'success', 'queries': optimized_queries, 'execution_plan': execution_plan}
-
-    except Exception as e:
-        return {'status': 'error', 'message': str(e)}
 
 
 @app.route('/optimize', methods=['POST'])
@@ -34,6 +15,7 @@ def get_optimized_query():
     try:
         query = request.json['query']
         execution_plan = None
+        optimized_execution_plan = None
         conn = db_util.get_connection()
         explanation = conn.execute(text('EXPLAIN FORMAT=TREE ' + query))
         for row in explanation:
@@ -41,7 +23,11 @@ def get_optimized_query():
         conn.close()
         result_df = search(query)
         optimized_queries = result_df["optimized_query"].tolist()
-        return {'status': 'success', 'queries': optimized_queries, 'execution_plan': execution_plan}
+        explanation = conn.execute(text('EXPLAIN FORMAT=TREE ' + optimized_queries[0]))
+        for row in explanation:
+            optimized_execution_plan = row[0]
+        return {'status': 'success', 'queries': optimized_queries, 'execution_plan_for_original': execution_plan,
+                'execution_plan_for_optimized': optimized_execution_plan}
 
     except Exception as e:
         return {'status': 'error', 'message': str(e)}
